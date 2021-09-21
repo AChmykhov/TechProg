f296eae2a443eee9a511769bace53ce023fc3b99
AChmykhov <alexander.chmykhov@gmail.com>
2021-09-21 20:23:39 +0300

    Init commit

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            var number = 2;
+            int count = 10;
+            float totalPrice = 20.59f;
+            char character = 'A';
+            string firstName = "Mosh";
+            bool isWorking = true;
+            Console.WriteLine(number);
+            Console.WriteLine(count);
+            Console.WriteLine(totalPrice);
+            Console.WriteLine(character);
+            Console.WriteLine(firstName);
+            Console.WriteLine(isWorking);
+        }
+    }
+}
