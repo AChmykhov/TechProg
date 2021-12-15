628ae34a81615d3bfa6b5498b54a6c4c2146ccfd
AChmykhov <alexander.chmykhov@gmail.com>
2021-12-15 11:38:16 +0300

    Added data population in FinalHomework.cs

--- Homework/FinalHomework.cs
+++ Homework/FinalHomework.cs
@@ -1,5 +1,7 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 
 namespace Homework
 {
@@ -11,8 +13,8 @@ namespace Homework
             public string Type;
             public int Cat;
             public List<string> Params;
-            public float Personal_cost;
-            public float b2b_cost;
+            public float PersonalCost;
+            public float b2bCost;
 
             public Product(string name, string type, int cat, List<string> pars, float personalCost, float b2bCost)
             {
@@ -20,28 +22,59 @@ namespace Homework
                 this.Type = type;
                 this.Cat = cat;
                 this.Params = pars;
-                this.Personal_cost = personalCost;
-                this.b2b_cost = b2bCost;
+                this.PersonalCost = personalCost;
+                this.b2bCost = b2bCost;
             }
         }
 
         struct Shop
         {
             public string Name;
+            public int Level;
             public List<List<Product>> Racks;
         }
 
         public void PopulateAndCalc()
         {
-            int num_shops = Convert.ToInt32(Console.ReadLine());
+            
+            List<Shop> shops = new List<Shop>();
 
-            List<Shop> shops = new List<Shop> ();
-            for (int i = 0; i < num_shops; i++)
+            
+            // Populate
+            /*
+             * Формат ввода:
+             * Число_магазинов
+             *      Имя_магазина Уровень_магазина Число_стоек
+             *          Число_товаров
+             *              Строка_товара
+             */
+            int numShops = Convert.ToInt32(Console.ReadLine());
+            for (int i = 0; i < numShops; i++)
             {
                 Shop tmp = new Shop();
-                tmp.Name = Console.ReadLine();
-                
+                string[] str = Console.ReadLine()!.Split();
+                tmp.Name = str[0];
+                tmp.Level = Convert.ToInt32(str[1]);
+                int numRacks = Convert.ToInt32(str[2]);
+                tmp.Racks = new List<List<Product>>();
+                for (int j = 0; j < numRacks; j++)
+                {
+                    List<Product> currRack = new List<Product>();
+                    int numProducts = Convert.ToInt32(Console.ReadLine());
+                    for (int k = 0; k < numProducts; k++)
+                    {
+                        string[] s = Console.ReadLine()!.Split();
+                        currRack.Add(new Product(s[0], s[1], Convert.ToInt32(s[2]), s[3].Split('_').ToList(),
+                            Convert.ToSingle(s[4]), Convert.ToSingle(s[5])));
+                    }
+
+                    tmp.Racks.Add(currRack);
+                }
+                shops.Add(tmp);
             }
+            
+            //Display
+            
         }
     }
 }
