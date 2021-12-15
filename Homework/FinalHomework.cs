2a2cf308365bef8b00e37dcd1f316c4dd0f30281
AChmykhov <alexander.chmykhov@gmail.com>
2021-12-15 12:09:24 +0300

    progress on calculating price in FinalHomework.cs

--- Homework/FinalHomework.cs
+++ Homework/FinalHomework.cs
@@ -36,10 +36,9 @@ namespace Homework
 
         public void PopulateAndCalc()
         {
-            
             List<Shop> shops = new List<Shop>();
 
-            
+
             // Populate
             /*
              * Формат ввода:
@@ -70,11 +69,60 @@ namespace Homework
 
                     tmp.Racks.Add(currRack);
                 }
+
                 shops.Add(tmp);
             }
-            
+
             //Display
+        }
+
+        private List<float> CalcPrice(Product product, int shopLevel, int fee)
+        {
+            List<float> Price = new List<float>();
+
+            float pCost = product.PersonalCost;
+            float bCost = product.b2bCost;
+            switch (shopLevel)
+            {
+                case 0:
+                    pCost *= 0.95f;
+                    bCost *= 0.95f;
+                    break;
+                case 1:
+                    pCost *= 0.90f;
+                    bCost *= 0.90f;
+                    break;
+                case 2:
+                    pCost *= 0.85f;
+                    bCost *= 0.85f;
+                    break;
+            }
+
+            Price.AddRange(new float[] {pCost, bCost, pCost, bCost});
+            switch (product.Cat)
+            {
+                case 0:
+                    Price[0] *= 0.95f;
+                    Price[1] *= 0.98f;
+                    Price[2] *= 0.94f;
+                    Price[3] *= 0.97f;
+                    break;
+                case 1:
+                    Price[0] *= 0.93f;
+                    Price[1] *= 0.96f;
+                    Price[2] *= 0.93f;
+                    Price[3] *= 0.95f;
+                    break;
+                case 2:
+                    Price[0] *= 0.90f;
+                    Price[1] *= 0.95f;
+                    Price[2] *= 0.89f;
+                    Price[3] *= 0.94f;
+                    break;
+            }
             
+
+            return Price;
         }
     }
 }
